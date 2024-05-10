2ac65acaa6dff987625766153a93b1ca298b8d39
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-05-10 05:56:48 +0300

    "Importing global CSS file using '@' shorthand in Next.js"
    This commit includes the following changes:
    
    * Added an import statement for the global CSS file using the '@' shorthand, which allows for

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Prompt",
+  description: "Discover & Share API Prompts",
+};
+
+const RootLayout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
